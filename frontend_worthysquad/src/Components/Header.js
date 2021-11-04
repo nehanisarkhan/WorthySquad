1a573d2de3955e69e18d03da5f82ef4fa685f944
faisalmirji123 <faisalmirji@gmail.com>
2021-11-04 16:40:28 +0530

    Added Changes to Navbar

--- frontend_worthysquad/src/Components/Header.js
+++ frontend_worthysquad/src/Components/Header.js
@@ -6,13 +6,19 @@ const Header = () => {
       <Navbar bg="light" variant="light" fixed="top">
         <Container>
           <Navbar.Brand href="#home">WorthySquad</Navbar.Brand>
-          <Nav className="d-flex justify-content-between">
+          <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#features">About</Nav.Link>
             <Nav.Link href="#events">Events</Nav.Link>
             <Nav.Link href="#donate">Donate</Nav.Link>
             <Nav.Link href="#volunteer">Volunteer</Nav.Link>
           </Nav>
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <Nav.Link href="Login">Login</Nav.Link>
+              <Nav.Link href="Sign-Up">Sign-Up</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
