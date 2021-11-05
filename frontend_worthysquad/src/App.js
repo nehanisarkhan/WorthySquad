703b2522f0936ddb80e598525a03cadd0e7db6ef
faisalmirji123 <faisalmirji@gmail.com>
2021-11-05 19:52:28 +0530

    Added images

--- frontend_worthysquad/src/App.js
+++ frontend_worthysquad/src/App.js
@@ -14,11 +14,25 @@ const App = () => {
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src="https://cdn.pixabay.com/photo/2021/08/28/19/13/wheat-field-6581723_960_720.jpg"
+              src="https://cdn.pixabay.com/photo/2017/02/08/17/24/fantasy-2049567_960_720.jpg"
               height="600px"
               width="350px"
               alt="First slide"
             />
+            <Carousel.Caption>
+              <h3> Plant trees and protect the environment</h3>
+              <p>Plantation Event</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src=" https://cdn.pixabay.com/photo/2020/04/17/08/11/blood-5053770_960_720.jpg"
+              height="600px"
+              width="350px"
+              alt="Second slide"
+            />
+
             <Carousel.Caption>
               <h3>Blood Donation Event</h3>
               <p>Donate Blood and save Lives</p>
@@ -27,21 +41,7 @@ const App = () => {
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src="https://cdn.pixabay.com/photo/2021/08/28/19/13/wheat-field-6581723_960_720.jpg"
-              height="600px"
-              width="350px"
-              alt="Second slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Plant trees and protect the environment</h3>
-              <p>Plantation Event</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://cdn.pixabay.com/photo/2021/08/28/19/13/wheat-field-6581723_960_720.jpg"
+              src="https://cdn.pixabay.com/photo/2015/10/25/07/09/hands-1005412_960_720.jpg"
               height="600px"
               width="350px"
               alt="Third slide"
