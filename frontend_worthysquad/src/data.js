8a9923464ae8d4cdb44b3106740cc0bf87ede18e
faisalmirji123 <faisalmirji@gmail.com>
2021-11-11 01:11:40 +0530

    Added Homepage

--- frontend_worthysquad/src/data.js
+++
@@ -1,6 +0,0 @@
-import image1 from "./Images/flowers.jpg";
-import image2 from "./Images/pexels-luis-del-río-15286.jpg";
-import multicar1 from "./Images/faisal.jpg";
-
-export const data = [image1, image2];
-export const multiData = [multicar1];
