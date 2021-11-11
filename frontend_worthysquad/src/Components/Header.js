8d348c76a088bfaca109e1aa72ba03d8543314d5
Neha Nisar Khan <nehaen1036@gmail.com>
2021-11-12 02:04:49 +0530

    added routes for login signup

--- frontend_worthysquad/src/Components/Header.js
+++ frontend_worthysquad/src/Components/Header.js
@@ -5,18 +5,18 @@ const Header = () => {
     <>
       <Navbar bg="light" variant="light" fixed="top">
         <Container>
-          <Navbar.Brand href="#home">WorthySquad</Navbar.Brand>
+          <Navbar.Brand href="/">WorthySquad</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#features">About</Nav.Link>
-            <Nav.Link href="#events">Events</Nav.Link>
+            <Nav.Link href="/eventlist">Events</Nav.Link>
             <Nav.Link href="#donate">Donate</Nav.Link>
             <Nav.Link href="#volunteer">Volunteer</Nav.Link>
           </Nav>
           <Navbar.Collapse className="justify-content-end">
             <Nav>
-              <Nav.Link href="Login">Login</Nav.Link>
-              <Nav.Link href="Sign-Up">Sign-Up</Nav.Link>
+              <Nav.Link href="/Login">Login</Nav.Link>
+              <Nav.Link href="/SignUp">Sign-Up</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
